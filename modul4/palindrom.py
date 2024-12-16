5e3a03c35c3abed724bb4ba72fcf1134670db3b7
1127-25-HTL3R <1127@htl.rennweg.at>
2024-12-16 14:48:00 +0100

    Added inital structure of the functions.

---
+++ modul4/palindrom.py
@@ -0,0 +1,45 @@
+__author__      = "Felix Friesenbichler"
+__email__       = "1127@htl.rennweg.at"
+__license__     = "GPLv2"
+
+
+def is_palindrom(s: str):
+    """
+    This method checks if the string s, which contains only one word, is a
+    palindrome.
+    """
+
+
+def is_palindrome_sentence(s: str):
+    """
+    Checks is the string s, which may contain a sentence, is a palindrome
+    sentence.
+    """
+
+
+def paindrome_product(x):
+    """
+    Finds the biggest palindrome number (smaller than x), which is the prodcut
+    of 3 digit number
+    """
+
+
+def get_dec_hex_palindrom(x):
+    """
+    Finds the biggest number (smaller than x), which is a palindrome in the
+    decimal- as well as in the hexadecimal system.
+    """
+
+
+def to_base(number: int, base: int) -> str:
+    """
+    :param number: Zahl im 10er-Style
+    :param base: Zielsystem (maximal 36)
+    :return: Zahl im Zielsystem als String
+
+    >>> to_base(1234,16)
+    '4D2'
+    """
+
+
+if __name__ == "__main__":
