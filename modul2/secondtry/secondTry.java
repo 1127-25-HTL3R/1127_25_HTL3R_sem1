3be0c025e7c02f2c7780e43d84b2c1abc5403d24
1127-25-HTL3R <1127@htl.rennweg.at>
2025-01-22 00:15:39 +0100

    UE02: Fixed test cases

--- modul2/secondtry/secondTry.java
+++ modul2/secondtry/secondTry.java
@@ -1,7 +1,7 @@
-package modul2.secondtry;
+package secondtry;
 
 import org.junit.*;
-import modul2.secondtry.*;
+import secondtry.finiteStateMachine;
 
 class secondTry {
 
@@ -12,78 +12,79 @@ class secondTry {
 	@Test
 	void test() {
 		// leicht
-        assertEquals(0, new finiteStateMachine().count(""));
-        assertEquals(0, new finiteStateMachine().count(" "));
-        assertEquals(0, new finiteStateMachine().count("  "));
+	finiteStateMachine FSM = new finiteStateMachine();
+	assert 0 == FSM.count("");
+	assert 0 == FSM.count(" ");
+	assert 0 == FSM.count("  ");
 
 
         // normal
-        assertEquals(1, new BezahlungFSM().count("one"));
-        assertEquals(1, new BezahlungFSM().count(" one"));
-        assertEquals(1, new BezahlungFSM().count("one "));
-        assertEquals(1, new BezahlungFSM().count(" one "));
-        assertEquals(1, new BezahlungFSM().count(" one  "));
-        assertEquals(1, new BezahlungFSM().count("  one "));
-        assertEquals(1, new BezahlungFSM().count("  one  "));
+	assert 1 == FSM.count("one");
+	assert 1 == FSM.count(" one");
+	assert 1 == FSM.count("one ");
+	assert 1 == FSM.count(" one ");
+	assert 1 == FSM.count(" one  ");
+	assert 1 == FSM.count("  one ");
+	assert 1 == FSM.count("  one  ");
 
-        assertEquals(1, new BezahlungFSM().count("one:"));
-        assertEquals(1, new BezahlungFSM().count(":one"));
-        assertEquals(1, new BezahlungFSM().count(":one:"));
-        assertEquals(1, new BezahlungFSM().count(" one  "));
-        assertEquals(1, new BezahlungFSM().count(" one : "));
-        assertEquals(1, new BezahlungFSM().count(": one :"));
-        assertEquals(3, new BezahlungFSM().count("ein erster Text"));
-        assertEquals(3, new BezahlungFSM().count(" ein  erster   Text      "));
-        assertEquals(3, new BezahlungFSM().count("ein:erster.Text"));
+	assert 1 == FSM.count("one:");
+	assert 1 == FSM.count(":one");
+	assert 1 == FSM.count(":one:");
+	assert 1 == FSM.count(" one  ");
+	assert 1 == FSM.count(" one : ");
+	assert 1 == FSM.count(": one :");
+	assert 3 == FSM.count("ein erster Text");
+	assert 3 == FSM.count(" ein erster    Text     ");
+	assert 3 == FSM.count("ein:erster.Text");
 
         // vielleicht falsch
-        assertEquals(1, new BezahlungFSM().count("a"));
-        assertEquals(1, new BezahlungFSM().count(" a"));
-        assertEquals(1, new BezahlungFSM().count("a "));
-        assertEquals(1, new BezahlungFSM().count(" a "));
+	assert 1 == FSM.count("a");
+	assert 1 == FSM.count(" a");
+	assert 1 == FSM.count("a ");
+	assert 1 == FSM.count(" a ");
 
         // mit html
-        assertEquals(1, new BezahlungFSM().count(" one  <html> "));
-        assertEquals(1, new BezahlungFSM().count(" one  < html> "));
-        assertEquals(1, new BezahlungFSM().count(" one  <html > "));
-        assertEquals(1, new BezahlungFSM().count(" one  < html > "));
-        assertEquals(4, new BezahlungFSM().count(" one <html> two<html>three <html> four"));
+	assert 1 == FSM.count(" one <html> ");
+	assert 1 == FSM.count(" one < html> ");
+	assert 1 == FSM.count(" one <html > ");
+	assert 1 == FSM.count(" one < html > ");
+	assert 4 == FSM.count(" one <html> two<html>three <html> four");
 
-        assertEquals(2, new BezahlungFSM().count(" one <html> two "));
-        assertEquals(2, new BezahlungFSM().count(" one <html>two "));
-        assertEquals(2, new BezahlungFSM().count(" one<html> two "));
-        assertEquals(2, new BezahlungFSM().count(" one<html>two "));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"xxx\" > two"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"xxx yyy\" > two"));
+	assert 2 == FSM.count(" one <html> two");
+	assert 2 == FSM.count(" one <html>two ");
+	assert 2 == FSM.count(" one<html> two ");
+	assert 2 == FSM.count(" one<html>two ");
+	assert 2 == FSM.count(" one<img alt=\"xxx\" > two");
+	assert 2 == FSM.count(" one<img alt=\"xxx yyy\" > two");
 
-        assertEquals(2, new BezahlungFSM().count(" one \"two\" "));
-        assertEquals(2, new BezahlungFSM().count(" one\"two\" "));
-        assertEquals(2, new BezahlungFSM().count(" one \"two\""));
-        assertEquals(3, new BezahlungFSM().count(" one \"two\"three"));
-        assertEquals(3, new BezahlungFSM().count(" one \"two\" three"));
+	assert 2 == FSM.count(" one \"two\" ");
+	assert 2 == FSM.count(" one\"two\" ");
+	assert 2 == FSM.count(" one \"two\"");
+	assert 3 == FSM.count(" one \"two\"three");
+	assert 3 == FSM.count(" one \"two\" three");
 
         // html - trickreich
         // Achtung: das ist teilweise nicht ganz legales HTML
-        assertEquals(1, new BezahlungFSM().count(" one<html")); // kein >
+	assert 1 == FSM.count(" one<html");
 
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild>\" > two")); // <> innerhalb ""
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"bild>\" > two")); // <> innerhalb ""
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild>\" keinwort> two"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild>\" src=\"bild.png\" >two"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bilFd\" keinwort>two"));
+	assert 2 == FSM.count(" one<img alt=\"<bild>\" > two");
+	assert 2 == FSM.count(" one<img alt=\"bild>\" > two");
+	assert 2 == FSM.count(" one<img alt=\"<bild>\" keinwort> two");
+	assert 2 == FSM.count(" one<img alt=\"<bild>\" src=\"bild.png\" > two");
+	assert 2 == FSM.count(" one<img alt=\"<bilFd\" keinwort> two");
 
-        assertEquals(1, new BezahlungFSM().count(" one<img alt=\"<bild\" keinwort"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild\" keinwort> two"));
-        assertEquals(1, new BezahlungFSM().count(" one<img alt=\"<bild keinwort> keinwort"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild keinwort keinwort\">two"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild keinwort< keinwort\">two"));
+	assert 1 == FSM.count(" one<img alt=\"<bild\" keinwort");
+	assert 2 == FSM.count(" one<img alt=\"<bild\" keinwort> two");
+	assert 1 == FSM.count(" one<img alt=\"<bild keinwort> keinwort");
+	assert 2 == FSM.count(" one<img alt=\"<bild keinwort keinwort\">two");
+	assert 2 == FSM.count(" one<img alt=\"<bild keinwort< keinwort\">two");
 
-        // ganz ganz fies -- \ entwertet das nÃ¤chste Zeichen
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild \\\" keinwort> keinwort\" keinwort>two"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild \\\" keinwort<keinwort\" keinwort>two"));
-        assertEquals(2, new BezahlungFSM().count(" one<img alt=\"<bild \\\" keinwort keinwort\" keinwort>two"));
+        // ganz ganz fies -- \ entwertet das nächste Zeichen
+	assert 2 == FSM.count(" one<img alt=\"<bild \\\" keinwort> keinwort\" keinwort>two");
+	assert 2 == FSM.count(" one<img alt=\"<bild \\\" keinwort<keinwort\" keinwort>two");
+	assert 2 == FSM.count(" one<img alt=\"<bild \\\" keinwort keinwort\" keinwort>two");
 
-        assertEquals(4, new BezahlungFSM().count(" \\\"null\\\" one<img alt=\"<bild \\\" keinwort keinwort\" keinwort>two \"three\""));
+	assert 4 == FSM.count(" \\\"null\\\" one<img alt=\"<bild \\\" keinwort keinwort\" keinwort>two \"three\"");
 	}
 
 }
