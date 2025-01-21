f69fbf869c6e7430984a07eedf1dc8a09a7c4b62
1127-25-HTL3R <1127@htl.rennweg.at>
2025-01-21 23:21:24 +0100

    UE02: Added function for checking if the currently iterated character is valid

---
+++ modul2/secondtry/finiteStateMachine.java
@@ -0,0 +1,62 @@
+package secondtry;
+
+/**
+  * A class for counting words.
+  * @author Felix Friesenbichler
+  */
+
+
+public class finiteStateMachine {
+
+	int counter;
+	public static int stringLength;
+	public static int iterationCounter;
+
+	enum State {
+
+		// Possible next word.
+		MIGHTBEWORD {
+			@Override
+
+		},
+		// New word
+		NEWWORD {
+			@Override
+		},
+		// if inside html-tag
+		INHTMLTAG {
+			@Override
+
+		},
+		// if in html-tag and escaped with '\'
+		INHTMLTAGESCAPED {
+			@Override
+		},
+		// if in html-tag and in '"' / block-quotes
+		INHTMLTAGQUOTE {
+			@Override
+		},
+		// if in html-tag and escaped and in block-quotes
+		INHTMLTAGQUOTEESCAPED {
+			@Override
+		}
+	};
+
+	/**
+	  * @param c current iterated character
+	  * @param context a finiteStateMachine
+	  * @return the current state
+	  */
+	abstract finiteStateMachine.State handleChar(char c, finiteStateMachine context);
+
+	/**
+	  * @param c currently iterated character
+	  * @return if it's a valid letter
+	  */
+	boolean isLetter(char c) {
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+                    || (c == 'Ä') || (c == 'Ö') || (c == 'Ü')
+                    || (c == 'ä') || (c == 'ö') || (c == 'ü') || (c == 'ß'
+            	);
+	}
+}
